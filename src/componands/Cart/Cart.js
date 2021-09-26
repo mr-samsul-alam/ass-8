63eae3278f7efba4b4f381343a78b6cd9726c7da
mr-samsul-alam <contactsamsulalam@gmail.com>
2021-09-26 18:38:14 +0600

    all new3

--- src/componands/Cart/Cart.js
+++ src/componands/Cart/Cart.js
@@ -1,19 +1,14 @@
 import React from 'react';
 
-
 const Cart = (props) => {
-   
     const { cart } = props || {}
-     
-
     const totalReducer = (previousValue, currentValue) => previousValue + currentValue.honorarium
     const total = cart.reduce(totalReducer, 0)
-
-
     return (
         <div>
             <h3><i className="fas fa-user-check"></i> Person Added : {cart.length}</h3>
             <h2>Total Cost: {total}</h2>
+            <h2>hellow</h2>
         </div>
     );
 };
